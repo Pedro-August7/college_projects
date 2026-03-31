99933c5ee714846de50877ff0224cc8e4dc496d4
Pedro <pedroaugusto77n@gmail.com>
2026-03-30 23:10:02 -0300

    Organiza estrutura do projeto e adiciona exercícios

---
+++ 2026/firts_trimester/at-react-fundamentals/src/exercises/01/components/container/container.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import FullScreen from "../../../../global_module/fullScreenContainer.module.css";
+
+export default function Container() {
+  return (
+    <>
+      <FullScreen></FullScreen>
+    </>
+  );
+}
---
+++ 2026/firts_trimester/at-react-fundamentals/src/exercises/01/ex01.jsx
@@ -0,0 +1,9 @@
+import Container from "./components//container/container";
+
+export default function Ex01() {
+  return (
+    <>
+      <Container />
+    </>
+  );
+}
